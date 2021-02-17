bf64da85ba1bfa194c2b4fc8176b917081d4e805
Olugbenga Ajayi <gbejula@gmail.com>
2021-02-17 03:01:15 +0100

    started with js

---
+++ script.js
@@ -0,0 +1,16 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+}
+
+updateCountdown();
